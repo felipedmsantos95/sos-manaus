b687404ab6c570fa469fe74abdb56d50f17feed2
felipedmsantos95 <fdms@icomp.ufam.edu.br>
2021-01-16 21:24:54 -0400

    backend ok

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(morgan('dev'))
 app.use(cors());
 app.use(express.json());
 app.use(routes);
